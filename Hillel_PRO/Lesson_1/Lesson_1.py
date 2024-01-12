25cade8c2a740635c1845289ba197f94f0f0bdac
Volodymyr Saratovskyy <sva.in.ua@gmail.com>
2024-01-12 12:44:25 +0100

    upd

--- Hillel_PRO/Lesson_1/Lesson_1.py
+++ Hillel_PRO/Lesson_1/Lesson_1.py
@@ -1,7 +1,7 @@
-#Hello Dima
+#Hello Dima!
 
-# It looks like I finished the configuration of PyCharm and VSCode with GitHub
+# It looks like I finished the configuration of PyCharm and VSCode with GitHub.
 
-# Have a nice day
+# Have a nice day!
 
 # Vladimir
