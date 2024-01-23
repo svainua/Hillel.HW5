a9ee14b31872ddb9987712534a9956b132e3b0a5
Volodymyr Saratovskyy <sva.in.ua@gmail.com>
2024-01-23 21:36:45 +0100

    upd

--- Hillel_PRO/Lesson_1/Lesson_1.py
+++
@@ -1,7 +0,0 @@
-#Hello Dima!!
-
-# It looks like I finished the configuration of PyCharm and VSCode with GitHub.
-
-# Have a nice day!
-
-# Vladimir.
--- Hillel_PRO/Lesson_3/main.py
+++
@@ -1,7 +0,0 @@
-def foo(name, middle_name, last_name):
-    pass
-
-def baz(num: int) -> str:
-    pass
-
-print("Hello1")
